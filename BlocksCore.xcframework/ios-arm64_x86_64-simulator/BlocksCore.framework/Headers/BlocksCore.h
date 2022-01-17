926d02a7883942a9324f0c51d3a45597e05e793f
Marvin Zhan <marvin.zhan@li.me>
2022-01-17 15:39:04 -0500

    add framework

---
+++ BlocksCore.xcframework/ios-arm64_x86_64-simulator/BlocksCore.framework/Headers/BlocksCore.h
@@ -0,0 +1,18 @@
+//
+//  BlocksCore.h
+//  BlocksCore
+//
+//  Created by Tengqi Zhan on 2021-09-22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BlocksCore.
+FOUNDATION_EXPORT double BlocksCoreVersionNumber;
+
+//! Project version string for BlocksCore.
+FOUNDATION_EXPORT const unsigned char BlocksCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlocksCore/PublicHeader.h>
+
+
